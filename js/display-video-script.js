fee4d26e1166715b3f5adffa7fd8fb5c0bbe22ca
sandrayousry <sandrayousry7@gmail.com>
2021-02-21 15:38:43 +0200

    bug in dashboard.js and display

--- js/display-video-script.js
+++ js/display-video-script.js
@@ -1,106 +1,82 @@
-           var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider;
-function intializePlayer(){
-  // Set object references
-  vid = document.getElementById("my_video");
-  playbtn = document.getElementById("playpausebtn");
-  seekslider = document.getElementById("seekslider");
-  curtimetext = document.getElementById("curtimetext");
-  durtimetext = document.getElementById("durtimetext");
-  mutebtn = document.getElementById("mutebtn");
-  volumeslider = document.getElementById("volumeslider");
-  // Add event listeners
-  playbtn.addEventListener("click",playPause,false);
-  seekslider.addEventListener("change",vidSeek,false);
-  vid.addEventListener("timeupdate",seektimeupdate,false);
-  mutebtn.addEventListener("click",vidmute,false);
-  volumeslider.addEventListener("change",setvolume,false);
-}
-window.onload = intializePlayer;
-function playPause(){
-  if(vid.paused){
-    vid.play();
-    playbtn.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    playbtn.innerHTML = "Play";
-  }
-}
-function vidSeek(){
-  var seekto = vid.duration * (seekslider.value / 100);
-  vid.currentTime = seekto;
-}
-function seektimeupdate(){
-  var nt = vid.currentTime * (100 / vid.duration);
-  seekslider.value = nt;
-  var curmins = Math.floor(vid.currentTime / 60);
-  var cursecs = Math.floor(vid.currentTime - curmins * 60);
-  var durmins = Math.floor(vid.duration / 60);
-  var dursecs = Math.floor(vid.duration - durmins * 60);
-  if(cursecs < 10){ cursecs = "0"+cursecs; }
-  if(dursecs < 10){ dursecs = "0"+dursecs; }
-  if(curmins < 10){ curmins = "0"+curmins; }
-  if(durmins < 10){ durmins = "0"+durmins; }
-  curtimetext.innerHTML = curmins+":"+cursecs;
-  durtimetext.innerHTML = durmins+":"+dursecs;
-}
-function vidmute(){
-  if(vid.muted){
-    vid.muted = false;
-    mutebtn.innerHTML = "Mute";
-  } else {
-    vid.muted = true;
-    mutebtn.innerHTML = "Unmute";
-  }
-}
-function setvolume(){
-  vid.volume = volumeslider.value / 100;
+/* Login function to get a new Token */
+async function login() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({ "username": "kareemdb5", "password": "1234" });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    let response = await fetch("https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/login", requestOptions);
+    let responseJsonObj = await response.json()
+    console.log(responseJsonObj.token)
+
+    return responseJsonObj.token
 }
 
+/* Get latest videos */
+async function renderLatestVideos() {
 
- async function login() {
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            var raw = JSON.stringify({ "username": "kareemdb5", "password": "1234" });
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-            let response = await fetch("https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/login", requestOptions);
-            let responseJsonObj = await response.json()
-             console.log(responseJsonObj.token)
-             return responseJsonObj.token
+    let dynamic_token = await login()
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("token", dynamic_token);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    let videoCount = 3;  // Maximum number of recent videos
+
+    for (let videoId = 0 ; videoId<videoCount ; videoId++) {
+
+        let httpResponse = await fetch(`https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/videos/${videoId}`,
+            requestOptions);
+
+        responseJsonObj = await httpResponse.json();
+        console.log(responseJsonObj)
+
+        // if no error occurs -> try section will run
+        try { 
+            if (responseJsonObj.data.url) {
+                renderVideo(videoId, responseJsonObj.data.url, responseJsonObj.data.title)
+            }
         }
-
-async function getVideo() {
-    let dynamic_token =await login()
-  console.log("token inside in get movies",dynamic_token) 
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("token", dynamic_token );
-
-let requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
- let  videoId =0     
-let httpResponse = await fetch(`https://damp-headland-40243.herokuapp.com/http://anyservice.imassoft.com/5/videos/${videoId}`, requestOptions)
-
-        responseJsonObj = await httpResponse.json();  
-        console.log(responseJsonObj) 
-
-          
-window.localStorage.setItem("video-url", responseJsonObj.data.url)   
+        // if error happens -> catch section will run
+        catch {
+            if (responseJsonObj.error) {
+                console.log("no urls found")
+                continue
+            }
+            else {
+                console.log("something else occured")
+                break
+            }
+        }
     }
- 
+}
 
-  getVideo();
-let localStorageUrl = window.localStorage.getItem("video-url")
+function renderVideo(id, src, title) {
 
+    let card = `<div class="card">
+                    <div class="row _thumbnail text-center"><video id="video-container-${id}" src="${src}" controls></video></div>
+                    <div class="row text-center"><h3>${title}</h3></div>
+                    <div class="row _text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper sapien non
+                        neque finibus, sit amet cursus est faucibus.</div>
+                    <div class="btn-group thumb-btn" role="group" aria-label="...">
+                        <button type="button" class="btn btn-default>View</button>
+                        <button type="button" class="btn btn-default">Star</button>
+                    </div>
+                    <span class="minutes">90 mins</span>
+                </div>`
+    
+    // cards_container = document.getElementById("cards_container")
+    // cards_container.innerHTML += card
 
-
-document.getElementById("my_video").setAttribute("src", localStorageUrl)
-
+    //console.log(cards_container)
+}
--- js/videos-dashboard-script.js
+++ js/videos-dashboard-script.js
@@ -1,85 +1,29 @@
-           var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider;
-function intializePlayer(){
-  // Set object references
-  vid = document.getElementById("my_video");
-  playbtn = document.getElementById("playpausebtn");
-  seekslider = document.getElementById("seekslider");
-  curtimetext = document.getElementById("curtimetext");
-  durtimetext = document.getElementById("durtimetext");
-  mutebtn = document.getElementById("mutebtn");
-  volumeslider = document.getElementById("volumeslider");
-  // Add event listeners
-  playbtn.addEventListener("click",playPause,false);
-  seekslider.addEventListener("change",vidSeek,false);
-  vid.addEventListener("timeupdate",seektimeupdate,false);
-  mutebtn.addEventListener("click",vidmute,false);
-  volumeslider.addEventListener("change",setvolume,false);
-}
-window.onload = intializePlayer;
-function playPause(){
-  if(vid.paused){
-    vid.play();
-    playbtn.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    playbtn.innerHTML = "Play";
-  }
-}
-function vidSeek(){
-  var seekto = vid.duration * (seekslider.value / 100);
-  vid.currentTime = seekto;
-}
-function seektimeupdate(){
-  var nt = vid.currentTime * (100 / vid.duration);
-  seekslider.value = nt;
-  var curmins = Math.floor(vid.currentTime / 60);
-  var cursecs = Math.floor(vid.currentTime - curmins * 60);
-  var durmins = Math.floor(vid.duration / 60);
-  var dursecs = Math.floor(vid.duration - durmins * 60);
-  if(cursecs < 10){ cursecs = "0"+cursecs; }
-  if(dursecs < 10){ dursecs = "0"+dursecs; }
-  if(curmins < 10){ curmins = "0"+curmins; }
-  if(durmins < 10){ durmins = "0"+durmins; }
-  curtimetext.innerHTML = curmins+":"+cursecs;
-  durtimetext.innerHTML = durmins+":"+dursecs;
-}
-function vidmute(){
-  if(vid.muted){
-    vid.muted = false;
-    mutebtn.innerHTML = "Mute";
-  } else {
-    vid.muted = true;
-    mutebtn.innerHTML = "Unmute";
-  }
-}
-function setvolume(){
-  vid.volume = volumeslider.value / 100;
-}
- async function login() {
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            var raw = JSON.stringify({ "username": "kareemdb5", "password": "1234" });
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-            let response = await fetch("https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/login", requestOptions);
-            let responseJsonObj = await response.json()
-             console.log(responseJsonObj.token)
-             return responseJsonObj.token
-        }
-
-
-async function getmovies() {
-
-  let dynamic_token =await login()
-  console.log("token inside in get movies",dynamic_token) 
+/* Login function to get a new Token */
+async function login() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("token",dynamic_token );
+    var raw = JSON.stringify({ "username": "kareemdb5", "password": "1234" });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    let response = await fetch("https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/login", requestOptions);
+    let responseJsonObj = await response.json()
+    console.log(responseJsonObj.token)
 
+    return responseJsonObj.token
+}
+
+/* Get latest videos */
+async function renderLatestVideos() {
+
+    let dynamic_token = await login()
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("token", dynamic_token);
 
     var requestOptions = {
         method: 'GET',
@@ -87,42 +31,56 @@ async function getmovies() {
         redirect: 'follow'
     };
 
-    let moivesCount = 3;
-    let urlArray = []
+    let videoCount = 10;  // Maximum number of recent videos
 
-    for(let moivesId=0 ; moivesId<moivesCount ; moivesId++) {
-        let url=`https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/videos-with-imgs/${moivesId}`
-        let httpResponse = await fetch(url,requestOptions);
-        
+    for (let videoId = 0 ; videoId<videoCount ; videoId++) {
 
-        responseJsonObj = await httpResponse.json();  
-        console.log(responseJsonObj) 
-  
-        urlArray.push(responseJsonObj.data.img_url)
-        window.localStorage.setItem("moives-url-array", urlArray) 
-window.localStorage.setItem("video_url",responseJsonObj.data.url )
+        let httpResponse = await fetch(`https://nameless-dusk-81295.herokuapp.com/http://anyservice.imassoft.com/5/videos/${videoId}`,
+            requestOptions);
+
+        responseJsonObj = await httpResponse.json();
+        console.log(responseJsonObj)
+
+        // if no error occurs -> try section will run
+        try { 
+            if (responseJsonObj.data.url) {
+                renderVideo(videoId, responseJsonObj.data.url, responseJsonObj.data.title)
+            }
+        }
+        // if error happens -> catch section will run
+        catch {
+            if (responseJsonObj.error) {
+                console.log("no urls found")
+                continue
+            }
+            else {
+                console.log("something else occured")
+                break
+            }
+        }
     }
 }
 
- 
+function renderVideo(id, src, title) {
 
+    let card = `<div class="card">
+                    <div class="row _thumbnail text-center"><video id="video-container-${id}" src="${src}" controls></video></div>
+                    <div class="row text-center"><h3>${title}</h3></div>
+                    <div class="row _text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper sapien non
+                        neque finibus, sit amet cursus est faucibus.</div>
+                    <div class="btn-group thumb-btn" role="group" aria-label="...">
+                        <button type="button" class="btn btn-default" onclick="redirection()">View</button>
+                        <button type="button" class="btn btn-default">Star</button>
+                    </div>
+                    <span class="minutes">90 mins</span>
+                </div>`
+    
+    cards_container = document.getElementById("cards_container")
+    cards_container.innerHTML += card
 
-getmovies();
-
-let localStorageUrlArray = window.localStorage.getItem("moives-url-array")
-let localStorageUrl = window.localStorage.getItem("video_url")
-
-localStorageUrlArray = localStorageUrlArray.split(",")
-console.log(localStorageUrlArray[0])
-console.log(localStorageUrlArray[1])
-console.log(localStorageUrlArray[2])
-
-document.getElementById("my_video").setAttribute("src",localStorageUrl)
-
-document.getElementById("movie_1").setAttribute("src", localStorageUrlArray[0])
-document.getElementById("movie_2").setAttribute("src", localStorageUrlArray[1])
-document.getElementById("movie_3").setAttribute("src", localStorageUrlArray[2])
-
-
-
-
+    //console.log(cards_container)
+}
+//function when click on "view" button will link to display-video page
+function redirection(){
+    window.location="display-video.html"
+}
